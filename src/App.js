a36a941dc0b916009fc21fc7395ed536dab82600
Tylercook2345 <tycook099@gmail.com>
2022-03-29 23:12:29 -0400

    React commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import About from './views/About';
+import Home from './views/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Tyler',
+      age: 300,
+      people: ['andrew','david','anthony','tyler','tommy','april','christopher','dylan','jamia']
+    }
+  }
+
+  happyBirthday = () => {
+    this.setState(
+      {age: this.state.age + 1}
+    )
+  };
+
+  componentDidMount () {
+    // console.log("MAIN APP: I have mounted")
+  }
+  
+
+
+
+
+
+  render() {
+    // console.log("MAIN APP: I have rendered")
+    return (
+      <div>
+        <Navbar myName={this.state.name} />
+        <h1>{this.state['name']} is {this.state.age} years old</h1>
+        <button onClick={()=>this.happyBirthday()}>Happy Birthday</button>
+        <Routes>
+          <Route path='/' element={<Home name = { this.state.name}/>}/>
+          <Route path='/about' element={<About myClass={this.state.people}/>}/>         
+        </Routes>
+      </div>
+    )
+  }
 }
 
-export default App;
